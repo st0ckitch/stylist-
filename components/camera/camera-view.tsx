993424a987ab1c89352b7bbba93aaca8fa0e5452
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2025-01-01 22:46:29 +0400

    Create camera-view.tsx

---
+++ components/camera/camera-view.tsx
@@ -0,0 +1,31 @@
+import { RefObject } from 'react'
+import Webcam from 'react-webcam'
+import { TorchButton } from './torch-button'
+import { CaptureButton } from './capture-button'
+
+interface CameraViewProps {
+  webcamRef: RefObject<Webcam>
+  torchOn: boolean
+  onTorchToggle: () => void
+  onCapture: () => void
+}
+
+export function CameraView({ webcamRef, torchOn, onTorchToggle, onCapture }: CameraViewProps) {
+  return (
+    <div className="relative">
+      <Webcam
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        className={`w-full aspect-[3/4] object-cover ${torchOn ? 'webcam-lighting' : ''}`}
+        videoConstraints={{
+          facingMode: 'user',
+          aspectRatio: 3/4,
+          width: { ideal: 1280 },
+          height: { ideal: 720 }
+        }}
+      />
+      <TorchButton active={torchOn} onClick={onTorchToggle} />
+      <CaptureButton onClick={onCapture} />
+    </div>
+  )
+}
