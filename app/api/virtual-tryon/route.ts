6e2e8df4a4ba592f6b7213448d7849dc53b50ceb
st0ckitch <135453997+st0ckitch@users.noreply.github.com>
2025-01-03 16:01:44 +0400

    Update route.ts

--- app/api/virtual-tryon/route.ts
+++ app/api/virtual-tryon/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { auth } from '@clerk/nextjs'
 
 export const runtime = 'nodejs'
-export const maxDuration = 60
+export const maxDuration = 300
 
 export async function POST(req: Request) {
   const { userId } = auth()
